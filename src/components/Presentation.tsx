71f091b22a746e0d5019b88d1cf23574730a4d2b
ezequielbugnon <bugnonezequiel@gmail.com>
2023-04-13 17:59:15 -0300

    add svg home

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -1,29 +1,32 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import people from '../assets/people.svg';
+import './style.css';
 
 const Presentation = () => {
   return (
     <Box
       sx={{ flexGrow: 1 }}
       display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
     >
+      <div>
       <Typography variant="h3" component="h2" paddingTop={5} paddingBottom={4}>
         Welcome to Interview Challenges Project
       </Typography>
-      <Typography variant="h5" component="h2" paddingBottom={3}>
+      <Typography variant="h6" component="h2" paddingBottom={3} >
         This Project is for the people who are interested in approving the job
         interview challenges especially those junior developers. Here you will
         find tests similar to those carried out by companies.
       </Typography>
       <a target="_blank" href={"https://github.com/ezequielbugnon/interview-challenges"} style={{textDecoration: "none"}}>
-        <Button variant="contained" size="large">
+        <Button variant="contained" size="large" style={{marginBottom: "40px"}}>
           Collaborate
         </Button>
       </a>
+      </div>
+    
+      <img src={people} alt="people" className="people-svg"/>
     </Box>
   );
 };
